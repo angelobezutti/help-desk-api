e0d971bfdb260d78cd7ac863ef8f9b2f67054132
angelobezutti <angeloantoniobzt@gmail.com>
2021-12-14 16:25:16 -0300

    Start Project

---
+++ src/test/java/com/bzt/helpdesk/HelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bzt.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
